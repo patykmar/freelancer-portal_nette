5c02fc4c538e0617e0a75daf44c6c6f71f275a55
Martin Patyk <martin.patyk@telia.no>
2021-10-23 16:03:03 +0200

    added common methods

--- app/model/BaseNDbModel.php
+++ app/model/BaseNDbModel.php
@@ -9,29 +9,56 @@
 namespace App\Model;
 
 use Nette;
+use Nette\Database\Context;
+use Nette\Database\SqlLiteral;
 use Nette\Database\Table\ActiveRow;
+use Nette\Database\Table\IRow;
 use Nette\Database\Table\Selection;
 
 abstract class BaseNDbModel extends Nette\Object
 {
-    /** @var Selection databaze incidentu */
+    /** @var string */
+    protected $tableName;
+
+    /** @var Context */
     private $database;
 
-    public function __construct(Selection $database)
+    public function __construct(Context $database)
     {
         $this->database = $database;
     }
 
     /**
-     * Vraci jeden zaznam ze zvolene tabulky.
+     * @param string $name
+     */
+    protected function setTableName($name)
+    {
+        $this->tableName = $name;
+    }
+
+    /**
      * @param int $id
      * @return ActiveRow
      */
     public function fetch($id)
     {
-        return $this->database->select('*')
-            ->wherePrimary($id)
-            ->limit(1)
-            ->fetch();
+        return $this->fetchAll()->get($id);
+    }
+
+    /**
+     * @return Selection
+     */
+    public function fetchAll()
+    {
+        return $this->database->table($this->tableName);
+    }
+
+    /**
+     * @param $values
+     * @return bool|int|SqlLiteral|IRow
+     */
+    public function insert($values)
+    {
+        return $this->fetchAll()->insert($values);
     }
 }
